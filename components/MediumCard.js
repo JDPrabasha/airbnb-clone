2c9184f21dcda6c7243e332b8c07601caf15ebb2
Dulaj <shad3s.of.m3@gmail.com>
2022-05-18 09:33:22 +0530

    complete hero, explore section

---
+++ components/MediumCard.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+function MediumCard({ image, title }) {
+  return (
+    <div className=" hover:scale-105 transform transition duration-300 cursor-pointer">
+      <div className="relative h-80 w-80 ease-out">
+        <Image src={image} className="rounded-lg" layout="fill" />
+      </div>
+
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+}
+
+export default MediumCard;
